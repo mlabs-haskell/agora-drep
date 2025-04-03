c95b1ad0b5ba73ed80f3c997bf570cba41d63f0e
Szabo Gergely <gege251@mailbox.org>
2025-04-03 19:32:49 +0200

    Implement further optimisations on the Voting effect

--- agora-drep-spec/Spec/Effects/Voting.hs
+++ agora-drep-spec/Spec/Effects/Voting.hs
@@ -1,6 +1,6 @@
 module Spec.Effects.Voting (spec) where
 
-import Agora.Effects.Voting (votingEffectValidator)
+import Agora.Effects.Voting (votingEffectScript)
 import Data.Text qualified as Text
 import Plutarch.Internal.Term qualified as Term
 import Spec.Effects.Voting.Context (certifyingContextSpec, spendingContextSpec, votingContextSpec)
@@ -19,7 +19,7 @@ spec =
   where
     script =
       either (error . Text.unpack) id $
-        Term.compile (Term.Tracing Term.LogInfo Term.DoTracing) votingEffectValidator
+        Term.compile (Term.Tracing Term.LogInfo Term.DoTracing) votingEffectScript
 
     -- 100 tests is way too small for a property test to search for a counterexample
     extraOptions :: QuickCheckTests -> QuickCheckTests
