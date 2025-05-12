08c33b8bb7d7ff4a96d13a46c328ac3a08bef097
Hercules CI Effects <hercules-ci[bot]@users.noreply.github.com>
2025-05-12 19:38:02 +0000

    Update gh-pages
    Store path: /nix/store/xxy0ihzn67y3rffmhrjlyf0zx83xs53n-website

--- toc.js
+++ toc.js
@@ -10,7 +10,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
     connectedCallback() {
         this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="introduction.html"><strong aria-hidden="true">1.</strong> Introduction</a></li><li class="chapter-item expanded "><a href="specification.html"><strong aria-hidden="true">2.</strong> Specification</a></li><li class="chapter-item expanded "><a href="haddock.html"><strong aria-hidden="true">3.</strong> Haddock</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
-        let current_page = document.location.href.toString().split("#")[0];
+        let current_page = document.location.href.toString().split("#")[0].split("?")[0];
         if (current_page.endsWith("/")) {
             current_page += "index.html";
         }
